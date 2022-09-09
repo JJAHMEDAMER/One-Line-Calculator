65ca872d3c84a1706a3a33cbf7835b185a588eed
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-09-09 19:57:32 +0200

    calculate again

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-from dataclasses import replace
+while True:
+    one_line = input("Evaluate: ")
+    one_line = one_line.replace(" ", "")
 
-
-one_line = input("Evaluate: ")
-one_line = one_line.replace(" ", "")
-
-if '+' in one_line:
-    i = one_line.find('+')
-    ans = int(one_line[:i]) + int(one_line[i+1:])
-elif '-' in one_line:
-    i = one_line.find('-')
-    ans = int(one_line[:i]) - int(one_line[i+1:])
-elif '*' in one_line:
-    i = one_line.find('*')
-    ans = int(one_line[:i]) * int(one_line[i+1:])
-elif '/' in one_line:
-    i = one_line.find('/')
-    ans = int(one_line[:i]) / int(one_line[i+1:])
+    if '+' in one_line:
+        i = one_line.find('+')
+        ans = int(one_line[:i]) + int(one_line[i+1:])
+    elif '-' in one_line:
+        i = one_line.find('-')
+        ans = int(one_line[:i]) - int(one_line[i+1:])
+    elif '*' in one_line:
+        i = one_line.find('*')
+        ans = int(one_line[:i]) * int(one_line[i+1:])
+    elif '/' in one_line:
+        i = one_line.find('/')
+        ans = int(one_line[:i]) / int(one_line[i+1:])
+        
+    print(ans)
     
-print(ans)
+    again = input("Calculate Again(y/n): ")
+    if again.lower() == 'n':
+        break
